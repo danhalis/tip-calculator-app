2c9a73c72da69f1fdac24a670e45cd644ca75353
Hieu Dao Le Duc <danhalisly@gmail.com>
2023-05-30 20:06:29 -0400

    Disable hover style on touch screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,13 @@ module.exports = {
   theme: {
     fontFamily: {
       "monospace": ['"Space Mono"']
+    },
+    extend: {
+      screens: {
+        // Disable hover style if it's a touch screen
+        // https://github.com/tailwindlabs/tailwindcss/discussions/1739#discussioncomment-56282
+        'betterhover': {'raw': '(hover: hover)'},
+      }
     }
   },
 }
